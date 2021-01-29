2bf6cc403f9d6a55d714a48a09bb801dc7e69575
DoctorSchu <78164347+DoctorSchu@users.noreply.github.com>
2021-01-30 00:32:23 +0100

    Speedboost Collectible

--- ZeroGravity/Assets/Scripts/AstronautMovement.cs
+++ ZeroGravity/Assets/Scripts/AstronautMovement.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class AstronautMovement : MonoBehaviour
 {
@@ -8,8 +10,9 @@ public class AstronautMovement : MonoBehaviour
     AstronautOxygen astronautOxygen;
 
     [SerializeField]
-    float force = 0.1f;
-    Vector2 forceVector;
+    float acceleration = 0.1f;
+    Vector2 accelerationVector;
+    float accelerationMultiplier = 1f;
 
     [SerializeField]
     float minimumSpeed = 0.1f;
@@ -17,7 +20,7 @@ public class AstronautMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        forceVector = new Vector2(force, force);
+        accelerationVector = new Vector2(acceleration, acceleration);
 
         astronautOxygen = GetComponent<AstronautOxygen>();
     }
@@ -29,13 +32,32 @@ public class AstronautMovement : MonoBehaviour
 
         if (Mathf.Abs(x.x) > 0f || Mathf.Abs(y.y) > 0f)
         {
-            Vector2 usedforce = (x + y).normalized * forceVector;
-            rb.AddForce(usedforce);
+            Vector2 usedforce = (x + y).normalized * accelerationVector;
+            rb.AddForce(usedforce * accelerationMultiplier);
             astronautOxygen.UseOxygen(usedforce.magnitude);
         }
-        else if(rb.velocity.magnitude < minimumSpeed)
+        else if (rb.velocity.magnitude < minimumSpeed)
         {
             rb.velocity = Vector2.zero;
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PowerUp"))
+        {
+            SpeedBoost collectible = collision.GetComponent<SpeedBoost>();
+            if (collectible != null)
+            {
+                StartCoroutine(SetAccelerationForSeconds(collectible.accelerationMultiplier, collectible.timeInSeconds));
+            }
+        }
+    }
+
+    private IEnumerator SetAccelerationForSeconds(float accelerationMultiplier, float timeInSeconds)
+    {
+        this.accelerationMultiplier = accelerationMultiplier;
+        yield return new WaitForSeconds(timeInSeconds);
+        this.accelerationMultiplier = 1f;
     }
 }
---
+++ ZeroGravity/Assets/Scripts/PowerUps/SpeedBoost.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoost : MonoBehaviour
+{
+    public float accelerationMultiplier = 5f;
+    public float timeInSeconds = 5f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // animate
+        Destroy(this.gameObject);
+    }
+}
