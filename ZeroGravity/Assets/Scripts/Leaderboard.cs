09966e4491df55e1d7598af315de9cb8ccd072fb
Jonas Kemper <jonas.kemper@digitalunit.berlin>
2021-01-30 01:40:00 +0100

    basic playfab integration

---
+++ ZeroGravity/Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+// reference: https://docs.microsoft.com/de-de/gaming/playfab/features/social/tournaments-leaderboards/quickstart
+public class Leaderboard : MonoBehaviour
+{
+
+    private static Leaderboard _instance;
+
+    public static Leaderboard Instance { get { return _instance; } }
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    //Get the players with the top 10 high scores in the game
+    public void RequestLeaderboard()
+    {
+        PlayFabClientAPI.GetLeaderboard(new GetLeaderboardRequest
+        {
+            StatisticName = "no2panic-leaderboard",
+            StartPosition = 0,
+            MaxResultsCount = 10
+        }, result => DisplayLeaderboard(result), FailureCallback);
+    }
+
+    private void DisplayLeaderboard(GetLeaderboardResult data)
+    {
+        Debug.Log(JsonUtility.ToJson(data));
+
+    }
+
+    public void SubmitScore(int playerScore)
+    {
+        PlayFabClientAPI.UpdatePlayerStatistics(new UpdatePlayerStatisticsRequest
+        {
+            Statistics = new List<StatisticUpdate> {
+            new StatisticUpdate {
+                StatisticName = "no2panic-leaderboard",
+                Value = playerScore
+            }
+        }
+        }, result => OnStatisticsUpdated(result), FailureCallback);
+    }
+
+    private void OnStatisticsUpdated(UpdatePlayerStatisticsResult updateResult)
+    {
+        Debug.Log("Successfully submitted high score");
+    }
+
+
+    private void FailureCallback(PlayFabError error)
+    {
+        Debug.LogWarning("Something went wrong with your API call. Here's some debug information:");
+        Debug.LogError(error.GenerateErrorReport());
+    }
+}
